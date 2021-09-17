c67aff7848e6eefe50b83ccfd41b1b9e1437faf0
Carlos Daniel Ortega Hernández <carlos.ortega.ast0318@gmail.com>
2021-09-17 13:02:02 -0500

    last changes

--- app.js
+++ app.js
@@ -1,84 +1,95 @@
-// <li class="collection-item">
-// 	<div><span>Buying food</span>
-// 	<i class="material-icons secondary-content">delete</i>
-// 	<a href="#modal1" class="modal-trigger secondary-content"> <i class="material-icons ">edit</i></a>
-// </div>
-// </li>
-
 const todoList = document.querySelector('#todo-list');
 const form = document.querySelector('#add-todo-form');
 const updateBtn = document.querySelector('#update');
+const logoutItems = document.querySelectorAll('.logged-out');
+const loginItems = document.querySelectorAll('.logged-in');
+let currentUser = null;
 let newTitle = '';
 let updateId = null;
 
-const renderList = (doc) => {
-	let li = document.createElement('li');
-	li.className = 'collection-item';
-	li.setAttribute('data-id', doc.id);
+function setupUI(user) {
+    if (user) {
+        loginItems.forEach(item => item.style.display = 'block');
+        logoutItems.forEach(item => item.style.display = 'none');
 
-	let div = document.createElement('div');
+    } else {
+        loginItems.forEach(item => item.style.display = 'none');
+        logoutItems.forEach(item => item.style.display = 'block');
+    }
+}
 
-	let title = document.createElement('span');
-	title.textContent = doc.data().title;
+function renderList(doc) {
+    let li = document.createElement('li');
+    li.className = "collection-item";
+    li.setAttribute('data-id', doc.id);
+    let div = document.createElement('div');
+    let title = document.createElement('span');
+    title.textContent = doc.data().title;
+    let anchor = document.createElement('a');
+    anchor.href = "#modal-edit";
+    anchor.className = "modal-trigger secondary-content";
+    let editBtn = document.createElement('i');
+    editBtn.className = "material-icons";
+    editBtn.innerText = "edit";
+    let deleteBtn = document.createElement('i');;
+    deleteBtn.className = "material-icons secondary-content";
+    deleteBtn.innerText = "delete";
+    anchor.appendChild(editBtn);
+    div.appendChild(title);
+    div.appendChild(deleteBtn);
+    div.appendChild(anchor);
+    li.appendChild(div);
+    deleteBtn.addEventListener('click', e => {
+        let id = e.target.parentElement.parentElement.getAttribute('data-id');
+        db.collection('alltodos').doc(currentUser.uid).collection('todos').doc(id).delete();
+    })
+    editBtn.addEventListener('click', e => {
+        updateId = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+    })
 
-	let anchor = document.createElement('a');
-	anchor.href = "#modal1";
-	anchor.className = 'modal-trigger secondary-content';
-	let editBtn = document.createElement('i');
-	editBtn.className = 'material-icons';
-	editBtn.innerText = 'edit';
+    todoList.append(li);
 
-	let deletBtn = document.createElement('i');
-	deletBtn.className = 'material-icons secondary-content';
-	deletBtn.innerText = 'delete';
-	deletBtn.style.cursor = 'pointer';
 
-	anchor.appendChild(editBtn);
-	div.appendChild(title);
-	div.appendChild(deletBtn);
-	div.appendChild(anchor);
-	li.appendChild(div);
-
-	deletBtn.addEventListener('click', e => {
-		let id = e.target.parentElement.parentElement.getAttribute('data-id');
-		db.collection('todos').doc(id).delete();
-	});
-
-	editBtn.addEventListener('click', e => {
-		updateId = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
-	});
-
-	todoList.append(li);
-};
-
-form.addEventListener('submit', e => {
-	e.preventDefault();
-
-	db.collection('todos').add({
-		title: form.title.value
-	})
-	form.title.value = '';
-});
-
+}
 updateBtn.addEventListener('click', e => {
-	newTitle = document.getElementsByName('newtitle')[0].value;
-	db.collection('todos').doc(updateId).update({
-		title: newTitle
-	})
-});
 
-db.collection('todos').orderBy('title').onSnapshot(snapshot => {
-	let changes = snapshot.docChanges();
-	changes.forEach(change =>  {
-		if(change.type == 'added'){
-			renderList(change.doc);
-		}else if(change.type == 'removed'){
-			let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
-			todoList.removeChild(li);			
-		}else if(change.type == 'modified'){
-			let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
-			li.getElementsByTagName('span')[0].textContent = newTitle;
-			newTitle = '';
-		}
-	});
-});
+    newTitle = document.getElementsByName('newtitle')[0].value;
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').doc(updateId).update({
+        title: newTitle
+    })
+})
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').add({
+        title: form.title.value
+    })
+    form.title.value = '';
+})
+function getTodos() {
+    todoList.innerHTML = '';
+    currentUser = auth.currentUser;
+
+    document.querySelector('#user-email').innerHTML = (currentUser != null ? currentUser.email : '');
+
+    console.log('currentUser', currentUser)
+    if (currentUser === null) {
+        todoList.innerHTML = '<h3 class="center-align">Please login to get todos</h3>';
+        return;
+    }
+    db.collection('alltodos').doc(currentUser.uid).collection('todos').orderBy('title').onSnapshot(snapshot => {
+        let changes = snapshot.docChanges()
+        changes.forEach(change => {
+            if (change.type == 'added') {
+                renderList(change.doc);
+
+            } else if (change.type == 'removed') {
+                let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
+                todoList.removeChild(li);
+            } else if (change.type == 'modified') {
+                let li = todoList.querySelector(`[data-id=${change.doc.id}]`);
+                li.getElementsByTagName('span')[0].textContent = newTitle;
+                newTitle = '';
+            }
+        });
+    })
+}
